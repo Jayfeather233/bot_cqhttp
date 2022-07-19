1fe84fab754e0f4360bab7d124649e8253c64fde
jayfe <jayfeather233@gmail.com>
2022-07-19 12:48:47 +0800

    支持了多层转发

--- src/function/autoForwardGenerator/AutoForwardGeneratorMain.java
+++ src/function/autoForwardGenerator/AutoForwardGeneratorMain.java
@@ -8,29 +8,26 @@ import com.alibaba.fastjson.JSONObject;
 import java.util.Scanner;
 
 public class AutoForwardGeneratorMain implements Processable {
-    @Override
-    public void process(String message_type, String message, long group_id, long user_id) {
-        message = message.substring(2);
-        Scanner S = new Scanner(message);
-        JSONArray JA = new JSONArray();
-        JSONObject J, J2;
-        while (S.hasNext()) {
-            String s1 = S.next();
-            String s2 = S.nextLine();
-            int kk = 0;
-            while (s2.charAt(kk) == ' ') kk++;
-            s2 = s2.substring(kk);
-            String uin;
-            if (s1.startsWith("[CQ:at,qq=")) {
-                s1 = s1.substring(10);
-                int i = 0;
-                while (i != s1.length() && s1.charAt(i) != ']') i++;
-                uin = s1.substring(0, i);
-                s1 = Main.getUserName(group_id, Long.parseLong(uin));
-            } else {
-                uin = s1;
-                s1 = Main.getUserName(group_id, Long.parseLong(s1));
-            }
+
+    private JSONObject getData(String s1, Scanner S, long group_id){
+        JSONObject J2 = new JSONObject();
+        String s2 = S.nextLine().trim();
+        String uin;
+        if (s1.startsWith("[CQ:at,qq=")) {
+            s1 = s1.substring(10);
+            int i = 0;
+            while (i != s1.length() && s1.charAt(i) != ']') i++;
+            uin = s1.substring(0, i);
+            s1 = Main.getUserName(group_id, Long.parseLong(uin));
+        } else {
+            uin = s1;
+            s1 = Main.getUserName(group_id, Long.parseLong(s1));
+        }
+        J2.put("name", s1);
+        J2.put("uin", uin);
+        if(s2.equals("转发")) {
+            J2.put("content", getContent(S,group_id));
+        } else {
             int pos = 0;
             do {
                 pos = s2.indexOf("[CQ:at,qq=", pos);
@@ -39,18 +36,36 @@ public class AutoForwardGeneratorMain implements Processable {
                 while (s2.charAt(pos) != ']') pos++;
                 s2 = s2.substring(0, pos) + ",name=" + Main.getUserName(group_id, Long.parseLong(s2.substring(po1, pos))) + s2.substring(pos);
             } while (true);
-
-            J = new JSONObject();
-            J2 = new JSONObject();
-            J2.put("name", s1);
-            J2.put("uin", uin);
             J2.put("content", s2);
-            J.put("type", "node");
-            J.put("data", J2);
+        }
+        return J2;
+    }
+
+
+    private JSONArray getContent(Scanner S, long group_id) {
+        String s1;
+        JSONArray JA = new JSONArray();
+        while(S.hasNext()){
+            s1 = S.next();
+            if(s1.equals("结束转发")){
+                break;
+            }
+            JSONObject J = new JSONObject();
+            J.put("type","node");
+            J.put("data",getData(s1, S, group_id));
             JA.add(J);
         }
+        return JA;
+    }
 
-        J = new JSONObject();
+    @Override
+    public void process(String message_type, String message, long group_id, long user_id) {
+
+        message = message.substring(2);
+        Scanner S = new Scanner(message);
+        JSONArray JA = getContent(S,group_id);
+
+        JSONObject J = new JSONObject();
         J.put("messages", JA);
 
         if(message_type.equals("group")){
